f1d499aabf94c36bdec4a2364bb8895f12a2c9d3
Hassan Chughtai <hassanchughtai@utexas.edu>
2018-12-01 20:12:11 -0600

    Initial commit

---
+++ Program3.java
@@ -0,0 +1,46 @@
+/*
+ * Name: <your name>
+ * EID: <your EID>
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+
+/**
+ * Your solution goes in this class.
+ * 
+ * Please do not modify the other files we have provided for you, as we will use
+ * our own versions of those files when grading your project. You are
+ * responsible for ensuring that your solution works with the original version
+ * of all the other files we have provided for you.
+ * 
+ * That said, please feel free to add additional files and classes to your
+ * solution, as you see fit. We will use ALL of your additional files when
+ * grading your solution.
+ */
+public class Program3 extends AbstractProgram3 {
+    /**
+     * Determines a solution to the optimal antenna range for the given input set. Study the
+     * project description to understand the variables which represent the input to your solution.
+     *
+     * @return Updated TownPlan town with the optimal solution
+     */
+    @Override
+    public TownPlan OptimalRange(TownPlan town) {
+        /* TODO implement this function */
+         return null; /* TODO remove this line */
+    }
+
+    /**
+     * Determines a solution to the set of base station positions that optimise antenna range for the given input set. Study the
+     * project description to understand the variables which } represent the input to your solution.
+     *
+     * @return Updaed TownPlan town with the optimal solution
+     */
+    @Override
+    public TownPlan OptimalPosBaseStations(TownPlan town) {
+        /* TODO implement this function */
+        return null; /* TODO remove this line */
+    }
+}
